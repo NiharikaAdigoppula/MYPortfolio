4375222f5fbdddf9aa69da986f910e9b228d94cb
Niharika Adigoppula <168505469+NiharikaAdigoppula@users.noreply.github.com>
2024-10-09 23:03:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    });
+  
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+  });
+  
